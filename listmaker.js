1a6bfb67e4215a219cfe76330f8aa51b9d40de66
Grace Brackman <gbrackma@purdue.edu>
2018-05-16 17:06:04 -0400

    have a working delete  button but does not work with arrays

--- listmaker.js
+++ listmaker.js
@@ -1,5 +1,6 @@
 
 const form = document.querySelector("#newRec")
+let array = new Array();
 
 form.addEventListener('submit', function(ev){
 
@@ -13,6 +14,7 @@ form.addEventListener('submit', function(ev){
     }
 
     allTheRec.appendChild(renderlist(newItem))
+    array.push(renderlist(newItem))
 
     form.reset()
     form.title.focus()
@@ -22,14 +24,58 @@ form.addEventListener('submit', function(ev){
 function renderlist(movie){
     
     const list = document.createElement('dl')
+    const dButton = document.createElement('button')
+    dButton.addEventListener('click', handleDelete)
+    dButton.textContent = 'Delete'
 
     //call renderListItem for name and age and color
     Object.keys(movie).map(function(label){
         const item = renderListItem(label, movie[label])
         list.appendChild(item);
+        list.appendChild(dButton)
     })
 
+    
+
     return list
+  }
+
+  function handleDelete(ev){
+
+      ev.preventDefault()
+      const thing = ev.target
+      //remove from array
+
+      debugger
+      //removeFromArray(thing)
+    let index = array.indexOf(thing.parentNode);
+    if (index > -1) {
+        array.splice(index, 1);
+    }   
+
+    thing.parentNode.remove()
+  }
+
+  function removeFromArray(item){
+
+      let indexOfGenre = item.parentNode.textContent.indexOf('Genre')
+      let title = item.parentNode.textContent.substring(5, indexOfGenre)
+      let genre = item.parentNode.textContent.substring( (indexOfGenre+5), (item.parentNode.textContent.length-6) )
+
+    const search = {
+        'Title': title,
+        'Genre': genre,
+    }
+
+
+    debugger
+    let index = array.indexOf(search);
+
+    if (index > -1) {
+        array.splice(index, 1);
+    }   
+    
+
   }
 
   function renderListItem(label, item){
